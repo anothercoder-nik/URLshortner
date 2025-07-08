91d91c6e5357f0dc86e8a57b73c668c5a7f15c59
Nik-_- <68611481+anothercoder-nik@users.noreply.github.com>
2025-07-09 02:02:36 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
                 </Link>
                 <button
                   onClick={handleLogout}
-                  className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md text-sm font-medium transition duration-300 shadow-sm"
+                  className="cursor-pointer bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md text-sm font-medium transition duration-300 shadow-sm"
                 >
                   Logout
                 </button>
