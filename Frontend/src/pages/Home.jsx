25e046dfedd1ec174779e341acd52901cdd1847c
Nik-_- <68611481+anothercoder-nik@users.noreply.github.com>
2025-07-09 01:59:31 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
     <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-      <h1 className="text-2xl font-bold text-center mb-6">URL Shortener</h1>
+      <h1 className="text-2xl font-bold text-center mb-6">One Link. Endless Reach.</h1>
       <UrlForm/>
     </div>
   </div>
